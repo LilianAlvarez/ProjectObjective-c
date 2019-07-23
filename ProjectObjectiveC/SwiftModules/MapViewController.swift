d6d1bb5b522ba870817d5c7f76f4d800cd02e418
Lilian Alvarez <lilian_alvarez_8@hotmail.com>
2019-07-24 00:18:18 +0200

    setup mapView

--- ProjectObjectiveC/SwiftModules/MapViewController.swift
+++ ProjectObjectiveC/SwiftModules/MapViewController.swift
@@ -6,4 +6,22 @@
 //  Copyright © 2019 Lilian ALVAREZ. All rights reserved.
 //
 
-class MapViewController: UIViewController {}
+import UIKit
+import MapKit
+
+class MapViewController: UIViewController {
+
+    @IBOutlet private weak var mapView: MKMapView!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setupMapView()
+    }
+
+    func setupMapView() {
+        let center = CLLocationCoordinate2D(latitude: CLLocationDegrees(45), longitude: CLLocationDegrees(4.5))
+        let span = MKCoordinateSpan(latitudeDelta: CLLocationDegrees(0.01), longitudeDelta: CLLocationDegrees(0.01))
+        let region = MKCoordinateRegion(center: center, span: span)
+        mapView.setRegion(region, animated: true)
+    }
+}
